3818c49320fefbb26692dd71465d505940fec555
Poojitha9312 <pujivasu2011@gmail.com>
2026-05-01 18:25:01 +0530

    done

--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,8 +1,7 @@
-class satya:
-    def __init__(x):
-        x.name=input("Please enter your name:")
-        x.age=int(input("please enter your age:"))
-    def sample(x):
-        print(f"My name is {x.name} and My age is{x.age}")
-a=satya()
-a.sample()
+class student:
+    satya=" Hi i am satya"
+    def sample(self):
+        print("hii")
+        # Accessing the dict attribute of class
+s=student()
+print(student.__dict__)
